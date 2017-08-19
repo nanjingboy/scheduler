3de76e2894b2e3e9ca363950882abf805239c65e
Tom.Huang <hzlhu.dargon@gmail.com>
2017-08-19 08:55:44 +0800

    complete crontab write logic

--- includes/cli.h
+++ includes/cli.h
@@ -12,6 +12,8 @@ private:
   static boost::filesystem::path parse_path(const std::map<std::string, std::string> & option_values);
   static std::string parse_user(const std::map<std::string, std::string> & option_values);
   static std::vector<std::string> parse_jobs_can_not_remove(const std::string & path, const std::string & user);
+  static std::string parse_schedule_time(const std::vector<int> & times, int min_value, int max_value);
+  static std::vector<std::string> parse_schedule_file(const std::string & path);
   static void write_crontab(const std::vector<std::string> & jobs, const std::string & user);
 
 public:
--- src/cli.cpp
+++ src/cli.cpp
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <boost/process.hpp>
+#include <json.hpp>
 
 #include "cli.h"
 
@@ -52,6 +53,85 @@ vector<string> Cli::parse_jobs_can_not_remove(const string & path, const string 
   return jobs;
 }
 
+string Cli::parse_schedule_time(const vector<int> & times, int min_value, int max_value) {
+  string result = "";
+  for (int time: times) {
+    if (time >= min_value && time <= max_value) {
+      result += to_string(time) + ",";
+    }
+  }
+  if (result != "") {
+    result.pop_back();
+    return result;
+  }
+
+  return "*";
+}
+
+vector<string> Cli::parse_schedule_file(const string & path) {
+  vector<string> jobs;
+  ifstream ifs(path);
+  if (ifs.is_open()) {
+    nlohmann::json json;
+    ifs >> json;
+    ifs.close();
+    if (json.is_array()) {
+      if (json.size() > 0) {
+        for (auto & element: json) {
+          string command = "";
+          if (element["command"].is_string()) {
+            command = element["command"];
+          }
+          if (command == "") {
+            continue;
+          }
+          string sections[] = { "*", "*", "*", "*", "*" };
+          if (element["minutes"].is_string()) {
+            sections[0] = element["minutes"];
+          } else if (element["minutes"].is_array()) {
+            sections[0] = parse_schedule_time(element["minutes"], 0, 59);
+          }
+
+          if (element["hours"].is_string()) {
+            sections[1] = element["hours"];
+          } else if (element["hours"].is_array()) {
+            sections[1] = parse_schedule_time(element["hours"], 0, 23);
+          }
+
+          if (element["daysOfTheMonth"].is_string()) {
+            sections[2] = element["daysOfTheMonth"];
+          } else if (element["daysOfTheMonth"].is_array()) {
+            sections[2] = parse_schedule_time(element["daysOfTheMonth"], 1, 31);
+          }
+
+          if (element["months"].is_string()) {
+            sections[3] = element["months"];
+          } else if (element["months"].is_array()) {
+            sections[3] = parse_schedule_time(element["months"], 1, 12);
+          }
+
+          if (element["daysOfTheWeek"].is_string()) {
+            sections[4] = element["daysOfTheWeek"];
+          } else if (element["daysOfTheWeek"].is_array()) {
+            sections[4] = parse_schedule_time(element["daysOfTheWeek"], 0, 7);
+          }
+
+          string job = "";
+          for (const string & section: sections) {
+            job += section + " ";
+          }
+          job += " " + command;
+          jobs.push_back(job);
+        }
+      }
+    } else {
+      cout << "Schedule file format is error: " << path << endl;
+      exit(1);
+    }
+  }
+  return jobs;
+}
+
 void Cli::write_crontab(const vector<string> & jobs, const string & user) {
   if (jobs.size() == 0) {
     process::system("sudo crontab -r -u " + user, process::std_err > process::null);
@@ -60,7 +140,7 @@ void Cli::write_crontab(const vector<string> & jobs, const string & user) {
   filesystem::path crontab_tmp_path = filesystem::temp_directory_path() /= "crontab.tmp";
   ofstream ofs(crontab_tmp_path.string());
   if (ofs.is_open()) {
-    for (string job: jobs) {
+    for (const string & job: jobs) {
       ofs << job << endl;
     }
     ofs.close();
@@ -95,9 +175,24 @@ void Cli::init(const map<string, string> & option_values) {
 }
 
 void Cli::write(const map<string, string> & option_values) {
-  cout << "call write..." << endl;
-  cout << "path:" << parse_path(option_values).string() << endl;
-  cout << "user:" << parse_user(option_values) << endl;
+  vector<string> schedule_file_jobs;
+  filesystem::path path = parse_path(option_values);
+  string path_string = filesystem::weakly_canonical(path).string();
+  if (filesystem::exists(path)) {
+    schedule_file_jobs = parse_schedule_file(path_string);
+    if (schedule_file_jobs.size() > 0) {
+      schedule_file_jobs.insert(schedule_file_jobs.begin(), "#BEGIN DEFINE CRON JOBS FROM:" + path_string);
+      schedule_file_jobs.push_back("#END DEFINE CRON JOBS FROM:" + path_string);
+    }
+  }
+
+  string user = parse_user(option_values);
+  vector<string> jobs = parse_jobs_can_not_remove(path_string, user);
+  for (const string & job: schedule_file_jobs) {
+    jobs.push_back(job);
+  }
+  write_crontab(jobs, user);
+  cout << "Crontab file has been written" << endl;
 }
 
 void Cli::clear(const map<string, string> & option_values) {
